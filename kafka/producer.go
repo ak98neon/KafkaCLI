ebd9a013620898e3d3016995b44c527ac9a23567
ak98neon <ak98nneon@gmail.com>
2020-05-28 14:02:31 +0300

    Remove unused functions

--- kafka/producer.go
+++ kafka/producer.go
@@ -1,62 +1,13 @@
 package kafka
 
 import (
-	"bytes"
 	"fmt"
 	"github.com/Shopify/sarama"
-	"math/rand"
-	"strconv"
-	"time"
 )
 
 var ProducerConfig = sarama.NewConfig()
 var BrokerList []string
 var ClientId string
-
-func PrepareMessageFromFile(buffer *bytes.Buffer, topic string) *sarama.ProducerMessage {
-	fmt.Println("File size:", len(buffer.Bytes()))
-	msg := &sarama.ProducerMessage{
-		Topic: topic,
-		Value: sarama.ByteEncoder(buffer.Bytes()),
-		Headers: []sarama.RecordHeader{
-			{
-				Key:   []byte("ORIG-TIME"),
-				Value: []byte(strconv.Itoa(int(time.Now().Unix()))),
-			},
-			{
-				Key:   []byte("ORIG-TTL"),
-				Value: []byte(strconv.Itoa(rand.Int())),
-			},
-			{
-				Key:   []byte("TRACE-ID"),
-				Value: []byte(strconv.Itoa(rand.Int())),
-			},
-		},
-	}
-	return msg
-}
-
-func PrepareMessageFromString(message string, topic string) *sarama.ProducerMessage {
-	msg := &sarama.ProducerMessage{
-		Topic: topic,
-		Value: sarama.ByteEncoder(message),
-		Headers: []sarama.RecordHeader{
-			{
-				Key:   []byte("ORIG-TIME"),
-				Value: []byte(strconv.Itoa(int(time.Now().Unix()))),
-			},
-			{
-				Key:   []byte("ORIG-TTL"),
-				Value: []byte(strconv.Itoa(rand.Int())),
-			},
-			{
-				Key:   []byte("TRACE-ID"),
-				Value: []byte(strconv.Itoa(rand.Int())),
-			},
-		},
-	}
-	return msg
-}
 
 func ProduceMessage(msg *sarama.ProducerMessage, count int, topic string) {
 	ProducerConfig.Version = sarama.V0_11_0_2
